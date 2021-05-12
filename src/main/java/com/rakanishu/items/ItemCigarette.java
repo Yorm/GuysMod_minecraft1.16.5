8e5c88b7da4afd7de505198ee4c68a781d67d9f5
Yorm <walterdive@gmail.com>
2021-05-12 13:32:46 +0300

    Added smoke direction

--- src/main/java/com/rakanishu/items/ItemCigarette.java
+++ src/main/java/com/rakanishu/items/ItemCigarette.java
@@ -8,10 +8,13 @@ import net.minecraft.item.*;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.*;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import static net.minecraft.entity.Entity.getHorizontalDistanceSqr;
 
 public class ItemCigarette extends Item implements IItemProvider{
     private static Logger logger = LogManager.getLogger(Item.class);
@@ -35,7 +38,7 @@ public class ItemCigarette extends Item implements IItemProvider{
                 flintAndSteel.hurtAndBreak(1, livingEntity, (consumer) -> {
                     consumer.broadcastBreakEvent(livingEntity.getUsedItemHand());
                 });
-                spawnParticle(livingEntity.getX(), livingEntity.getY(), livingEntity.getZ(),  world,livingEntity.xRot,livingEntity.yRot);
+                spawnParticle(livingEntity.getX(), livingEntity.getY(), livingEntity.getZ(), world, livingEntity);
                 world.playSound((PlayerEntity)null, livingEntity.getX(), livingEntity.getY(), livingEntity.getZ(), SoundEvents.PARROT_IMITATE_CREEPER, SoundCategory.PLAYERS, 0.5F, world.random.nextFloat() * 0.1F + 0.9F);
             }
         //}
@@ -47,7 +50,7 @@ public class ItemCigarette extends Item implements IItemProvider{
 
         //if(livingEntity instanceof PlayerEntity){
             if(!flintAndSteel.isEmpty() && flintAndSteel.getItem() instanceof FlintAndSteelItem) {
-                spawnParticle(livingEntity.getX(), livingEntity.getY(), livingEntity.getZ(),  world,livingEntity.xRot,livingEntity.yRot);
+                spawnParticle(livingEntity.getX(), livingEntity.getY(), livingEntity.getZ(),  world,livingEntity);
                 flintAndSteel.hurtAndBreak(1, livingEntity, (consumer) -> {
                     consumer.broadcastBreakEvent(livingEntity.getUsedItemHand());
                 });
@@ -75,44 +78,19 @@ public class ItemCigarette extends Item implements IItemProvider{
         }
     }
 
-    private void spawnParticle(double xCoord, double yCoord, double zCoord, World world, float xRot, float yRot){
-        for (int k = 0; k < 12; ++k){
-            world.addParticle(ParticleTypes.SMOKE, xCoord , yCoord + 1.5D, zCoord , 0.05D, 0.02D,0.05D);
+    private void spawnParticle(double xCoord, double yCoord, double zCoord, World world, LivingEntity livingEntity){
+        Vector3d vector3d = livingEntity.getViewVector(1.0F); // returns a normalized vector, which is what you want in this case
+        // add one of each vector coordinate to the player's position to get a position just in front of the player
+        double x = xCoord + vector3d.x;
+        double y = yCoord + vector3d.y;
+        double z = zCoord + vector3d.z;
+        for (int k = 0; k < 3; ++k){
+            double d1 = (double)x + world.random.nextDouble()-0.4d;
+            double d2 = (double)y + world.random.nextDouble()+1.0d;
+            double d3 = (double)z + world.random.nextDouble();
+            world.addParticle(ParticleTypes.SMOKE,  d1, d2, d3,vector3d.x * 0.15, 0.05D,vector3d.z * 0.15);
+            world.addParticle(ParticleTypes.CAMPFIRE_SIGNAL_SMOKE, d1, d2, d3, vector3d.x * 0.15 , 0.05D,vector3d.z * 0.15);
+            world.addParticle(ParticleTypes.LARGE_SMOKE, d1, d2, d3, vector3d.x * 0.15 , 0.05D,vector3d.z * 0.15);
         }
-        world.addParticle(ParticleTypes.CAMPFIRE_SIGNAL_SMOKE, xCoord , yCoord + 1.5D, zCoord , 0.05D, 0.02D,0.05D);
-
-
-        /*
-
-        logger.info("xrot " +xRot);//up down -90  90
-        logger.info("yrot " +yRot); //circle -180  180
-
-
-
-        float f = -MathHelper.sin(yRot * ((float)Math.PI / 180F)) * MathHelper.cos(xRot * ((float)Math.PI / 180F));
-        float f1 = -MathHelper.sin((xRot +yRot) * ((float)Math.PI / 180F));
-        float f2 = MathHelper.cos(yRot * ((float)Math.PI / 180F)) * MathHelper.cos(xRot * ((float)Math.PI / 180F));
-
-        logger.info("f  " +f);
-        logger.info("f1  " +f1);
-        logger.info("f2  " +f2);
-
-
-        Vector3d vector3d = (new Vector3d(f, f1, f2)).normalize().add(this.random.nextGaussian() * (double)0.0075F * (double)1.0F, this.random.nextGaussian() * (double)0.0075F * (double)1.0F, this.random.nextGaussian() * (double)0.0075F * (double)1.0F).scale((double)f * 3.0F);
-        logger.info("f2  " +f2);
-
-        float fs = MathHelper.sqrt(getHorizontalDistanceSqr(vector3d));
-        yRot = (float)(MathHelper.atan2(vector3d.x, vector3d.z) * (double)(180F / (float)Math.PI));
-        xRot = (float)(MathHelper.atan2(vector3d.y, (double)fs) * (double)(180F / (float)Math.PI));
-
-
-        world.addParticle(ParticleTypes.CAMPFIRE_SIGNAL_SMOKE, xCoord  , yCoord + 1.5D, zCoord, 0.05D, 0.02D,0.05D);
-        */
     }
-
-    /*
-    public static double getHorizontalDistanceSqr(Vector3d p_213296_0_) {
-        return p_213296_0_.x * p_213296_0_.x + p_213296_0_.z * p_213296_0_.z;
-    }
-    */
 }
